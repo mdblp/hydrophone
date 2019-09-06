4ef7131230bce3b10c39297b9ac0b86ef45932ce
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2019-09-06 12:14:11 +0000

    PT-311 Fix unit tests

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -eu
 
+export TEMPLATE_PATH="$(dirname $(realpath $0))/templates"
+echo "TEMPLATE_PATH=${TEMPLATE_PATH}"
 for D in $(find . -name '*_test.go' ! -path './vendor/*' | cut -f2 -d'/' | uniq); do
+    echo "${D}"
     (cd ${D}; go test -v)
 done
