11b8b3caa6348e025a080069e653fbb6475b301d
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2019-09-06 12:25:40 +0000

    PT-311 Fix test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-export TEMPLATE_PATH="$(dirname $(realpath $0))/templates"
+export TEMPLATE_PATH="$(dirname $(readlink -f $0))/templates"
 echo "TEMPLATE_PATH=${TEMPLATE_PATH}"
 for D in $(find . -name '*_test.go' ! -path './vendor/*' | cut -f2 -d'/' | uniq); do
     echo "${D}"
