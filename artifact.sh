5f0076ef6ede4909bbb085efa89ea150279ca87e
Fabrice SOUGEY <fabrice@diabeloop.fr>
2020-02-24 19:49:05 +0100

    Test adding openapi generation and deployment in pipeline

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/dblp/artifact/artifact_go.sh'
+wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add_openapi_go/artifact/artifact_go.sh'
 chmod +x artifact_go.sh
 
 . ./version.sh
