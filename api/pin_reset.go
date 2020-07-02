475b7fa5cad5ebc216acafe0d6a3ec0b595e87df
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-07-02 18:44:04 +0200

    Minor changes

--- api/pin_reset.go
+++ api/pin_reset.go
@@ -36,6 +36,7 @@ const (
 // @Failure 422 {object} status.Status "Error when sending the email (probably caused by the mailing service)"
 // @Failure 500 {object} status.Status "Internal error while processing the request, detailed error returned in the body"
 // @Router /send/pin-reset/{userid} [post]
+// @security TidepoolAuth
 func (a *Api) SendPinReset(res http.ResponseWriter, req *http.Request, vars map[string]string) {
 
 	// by default, language is EN. It will be overriden if prefered language is found later
@@ -44,10 +45,9 @@ func (a *Api) SendPinReset(res http.ResponseWriter, req *http.Request, vars map[
 	var usrDetails *shoreline.UserData
 	var err error
 
-	var token = req.Header.Get(TP_SESSION_TOKEN)
-
-	td := a.sl.CheckToken(token)
-	if td == nil || td.IsServer {
+	if td := a.token(res, req); td == nil {
+		return
+	} else if td.IsServer {
 		a.sendError(res, http.StatusForbidden, statusPinResetNoServer)
 		return
 	}
@@ -82,7 +82,8 @@ func (a *Api) SendPinReset(res http.ResponseWriter, req *http.Request, vars map[
 	// first get the IMEI of the patient's handset
 	var patientConfig *portal.PatientConfig
 
-	if patientConfig, err = a.portal.GetPatientConfig(token); err != nil {
+	// actual token value is needed to call portal client: we know it does exist and is a user one
+	if patientConfig, err = a.portal.GetPatientConfig(req.Header.Get(TP_SESSION_TOKEN)); err != nil {
 		a.sendError(res, http.StatusInternalServerError, statusPinResetErr, "error getting patient config: ", err.Error())
 		return
 	}
