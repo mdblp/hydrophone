738cb28208b98a87a37402bfc8b8a0740ee836b6
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-08-13 15:44:08 +0200

    Test new artifact with no change

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/dblp/artifact/artifact_go.sh'
+wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/YLP-126/artifact/artifact_go.sh'
 chmod +x artifact_go.sh
 
 . ./version.sh
