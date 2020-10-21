01f069fe74e3ef20d5e3ad953fe3c78c570c7b6d
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-10-21 10:39:27 +0200

    Test new security scan

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/review_security_scan/artifact/artifact_go.sh'
+wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add-platform-project/artifact/artifact_go.sh'
 chmod +x artifact_go.sh
 
 . ./version.sh
