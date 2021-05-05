81ed484bde74482423499d133f8d0104a523af36
franck <franck@diabeloop.fr>
2021-05-05 18:37:25 +0200

    add team information in confiramtions

--- clients/mockStoreClient.go
+++ clients/mockStoreClient.go
@@ -88,31 +88,31 @@ func (d *MockStoreClient) FindConfirmation(ctx context.Context, notification *mo
 	if notification.Key == "invite.wrong.type" {
 		notification.Status = "pending"
 		notification.Type = "a.wrong.type"
-		notification.TeamID = "123456"
+		notification.Team.TeamID = "123456"
 		notification.UserId = "123.456.789"
 	}
 	if notification.Key == "medicalteam.invite.member" {
 		notification.Status = "pending"
 		notification.Type = "medicalteam_invitation"
-		notification.TeamID = "123456"
+		notification.Team.TeamID = "123456"
 		notification.UserId = "123.456.789"
 	}
 	if notification.Key == "medicalteam.invite.wrong.member" {
 		notification.Status = "pending"
 		notification.Type = "medicalteam_invitation"
-		notification.TeamID = "123456"
+		notification.Team.TeamID = "123456"
 		notification.UserId = "not.my.id"
 	}
 	if notification.Key == "medicalteam.invite.patient" {
 		notification.Status = "pending"
 		notification.Type = "medicalteam_patient_invitation"
-		notification.TeamID = "123456"
+		notification.Team.TeamID = "123456"
 		notification.UserId = "123.456.789"
 	}
 	if notification.Key == "invalid.key" {
 		notification.Status = ""
 		notification.Type = ""
-		notification.TeamID = ""
+		notification.Team.TeamID = ""
 		notification.UserId = ""
 	}
 	if notification.Key == "key.does.not.exist" {
