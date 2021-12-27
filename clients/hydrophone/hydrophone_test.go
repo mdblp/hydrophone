75db5213c74361a5722d665df3bc5608d1b20668
Yann Blanc <yann.blanc@diabeloop.fr>
2021-12-27 10:18:12 +0100

    add test for cancel client

--- clients/hydrophone/hydrophone_test.go
+++ clients/hydrophone/hydrophone_test.go
@@ -15,25 +15,35 @@ func buildServer(t *testing.T, userID string, testToken string, confirmType stri
 	return httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		urlPath := req.URL.Path
 		if strings.HasPrefix(urlPath, "/"+confirmType+"/") {
-			if req.Method != "GET" {
+			if req.Method != "GET" && req.Method != "PUT" {
 				t.Errorf("Incorrect HTTP Method [%s]", req.Method)
 			} else if req.Header.Get("x-tidepool-session-token") != testToken {
 				res.WriteHeader(http.StatusUnauthorized)
 			} else {
 				userID = strings.TrimPrefix(urlPath, "/"+confirmType+"/")
-				switch userID {
-				case "authorizedWithData":
-					res.WriteHeader(http.StatusOK)
-					if confirmType == "invite" {
-						fmt.Fprint(res, `[{"key":"key1","type":"medicalteam_invitation"}, {"key":"key2","type":"medicalteam_do_admin"}]`)
-					} else {
-						fmt.Fprint(res, `[{"key":"key3","type":"signup_confirmation"}]`)
+				if req.Method == "GET" {
+					switch userID {
+					case "authorizedWithData":
+						res.WriteHeader(http.StatusOK)
+						if confirmType == "invite" {
+							fmt.Fprint(res, `[{"key":"key1","type":"medicalteam_invitation"}, {"key":"key2","type":"medicalteam_do_admin"}]`)
+						} else {
+							fmt.Fprint(res, `[{"key":"key3","type":"signup_confirmation"}]`)
+						}
+					case "authorizedWithoutData":
+						res.WriteHeader(http.StatusNotFound)
+					case "error":
+						res.WriteHeader(http.StatusInternalServerError)
 					}
-				case "authorizedWithoutData":
-					res.WriteHeader(http.StatusNotFound)
-				case "error":
-					res.WriteHeader(http.StatusInternalServerError)
+				} else {
+					switch userID {
+					case "error":
+						res.WriteHeader(http.StatusInternalServerError)
+					default:
+						res.WriteHeader(http.StatusOK)
+					}
 				}
+
 			}
 		} else {
 			t.Errorf("Unknown path[%s]", urlPath)
@@ -91,6 +101,32 @@ func TestGetPendingSignup(t *testing.T) {
 	testWrongToken(t, hydrophoneClient, "signup")
 	testEmptyData(t, hydrophoneClient, testToken, "signup")
 	testError(t, hydrophoneClient, testToken, "signup")
+}
+
+func TestCancelSignup(t *testing.T) {
+	testToken := "a.b.c"
+	var userID string
+	srvr := buildServer(t, userID, testToken, "signup")
+	defer srvr.Close()
+
+	hydrophoneClient := NewHydrophoneClientBuilder().
+		WithHost(srvr.URL).
+		Build()
+
+	err := hydrophoneClient.CancelSignup(models.Confirmation{UserId: "randomUserId"}, testToken)
+	if err != nil {
+		t.Errorf("Failed CancelSignup with error[%v]", err)
+	}
+
+	err = hydrophoneClient.CancelSignup(models.Confirmation{UserId: "randomUserId"}, "wrongToken")
+	if err == nil {
+		t.Error("Unauthorized request should return an error")
+	}
+
+	err = hydrophoneClient.CancelSignup(models.Confirmation{UserId: "error"}, testToken)
+	if err == nil {
+		t.Error("Error from service should be forwarded")
+	}
 }
 
 func testWrongToken(t *testing.T, hydrophoneClient *Client, confirmType string) {
