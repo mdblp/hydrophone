c6b6453ce57a053ef6297a3d76d7274f3e0891f7
Nasser <nasser.srairi@diabeloop.fr>
2025-11-21 12:01:56 +0100

    fix: wrong testresult file name

--- test.sh
+++ test.sh
@@ -2,20 +2,25 @@
 
 export TEMPLATE_PATH="$(dirname $(readlink -f $0))/templates"
 
+echo "Running tests..."
 go install github.com/jstemmer/go-junit-report@latest
 go install github.com/t-yuki/gocover-cobertura@latest
+echo "Tidying modules..."
 go mod tidy
+echo "Running tests with coverage..."
 go test -v -race -coverprofile=coverage.out.tmp ./... 2>&1 > testresults.txt
+echo "Generating coverage report..."
 cat coverage.out.tmp | grep -v "_test" | grep -v "mocks" | grep -v "docs" > coverage.out
 testPass=$?
-cat test-report.txt
-cat test-report.txt | go-junit-report  > test-report.xml
+echo "Generating test report..."
+cat testresults.txt
+cat testresults.txt | go-junit-report  > test-report.xml
 
 if [ $testPass -eq 1 ]; then
   echo "Test failed"
 fi
-
+echo "Generating coverage XML..."
 gocover-cobertura < coverage.out > coverage.xml
 go tool cover -html='coverage.out' -o coverage.html
-
+echo "Test completed."
 exit $testPass
