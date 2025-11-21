292c442d12bced3440db12cf4ffd660c2505a5ba
Nasser <nasser.srairi@diabeloop.fr>
2025-11-21 10:33:48 +0100

    fix pipe by adding go mod tidy in test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ export TEMPLATE_PATH="$(dirname $(readlink -f $0))/templates"
 
 go install github.com/jstemmer/go-junit-report@latest
 go install github.com/t-yuki/gocover-cobertura@latest
+go mod tidy
 go test -v -race -coverprofile=coverage.out.tmp ./... 2>&1 > testresults.txt
 cat coverage.out.tmp | grep -v "_test" | grep -v "mocks" | grep -v "docs" > coverage.out
 testPass=$?
